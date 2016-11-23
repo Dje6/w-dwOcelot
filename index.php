ce736aa15f65166a0d5345d7257ab502b7a32575
DWOcelot <benoit.blondel12@gmail.com>
2016-11-23 12:11:36 +0100

    ajout h1 et background color pink

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title></title>
+  </head>
+  <body>
+
+    <h1>JESSY JTM</h1>
+
+  </body>
+</html>
