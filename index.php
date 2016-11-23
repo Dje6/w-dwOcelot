2c341aebf285a0d0796902ae623a764c31f5c829
Jessy <jessy35370@gmail.com>
2016-11-23 12:31:25 +0100

    Ajout de bootstrap et création de formulaire sans style

--- index.php
+++ index.php
@@ -10,11 +10,20 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
     <title></title>
   </head>
 
   <body>
     <h1>JESSY JTM</h1>
+
+    <form class="" action="" method="POST">
+      <label for="name">Nom :</label><br>
+      <input type="text" name="name" value=""><br>
+      <label for="prenom">Prénom : </label><br>
+      <input type="text" name="prenom" value=""><br>
+      <input type="submit" name="submit" value="ASV ?">
+    </form>
   </body>
 </html>
