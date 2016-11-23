19bbf11c25670ad3ca4d17f2ee5444c00f7e222b
DWOcelot <benoit.blondel12@gmail.com>
2016-11-23 14:42:28 +0100

    modif titre de page

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ if (!empty($_POST['submit'])) {
 
     <div class="container-fluid">
       <div class="row">
-        <h1 class="titrePage">JESSY JTM</h1>
+        <h1 class="titrePage">JESSY JTM PLUS QUE TOUT</h1>
       </div>
     </div>
 
