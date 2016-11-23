bfaabb9d4e0ba74173f37d80ab7ba60882f1cfaa
Jessy <jessy35370@gmail.com>
2016-11-23 12:54:47 +0100

    Gestion des erreurs en PHP

--- index.php
+++ index.php
@@ -1,6 +1,38 @@
 <?php
 
+include 'include/functions.php';
 
+$error = array();
+
+
+if (!empty($_POST['submit'])) {
+  $name = trim(strip_tags($_POST['name']));
+  $prenom = trim(strip_tags($_POST['prenom']));
+
+  if (!empty($name)) {
+    if (strlen($name) < 3) {
+      $error['name'] = 'Veuillez indiquer un nom plus long';
+    } elseif (strlen($name) > 20) {
+      $error['name'] = 'Veuillez indiquer un nom plus court';
+    }
+  } else {
+    $error['name'] = 'Veuillez indiquer un nom';
+  }
+
+  if (!empty($prenom)) {
+    if (strlen($prenom) < 3) {
+      $error['prenom'] = 'Veuillez indiquer un prenom plus long';
+    } elseif (strlen($prenom) > 40) {
+      $error['prenom'] = 'Veuillez indiquer un prenom plus long';
+    }
+  } else {
+    $error['prenom'] = 'Veuillez indiquer un prénom';
+  }
+
+  if (count($error) == 0) {
+    echo 'HEY BIENVENUE CHEZ LES ROUGES';
+  }
+}
 
 ?>
 
@@ -20,8 +52,10 @@
 
     <form class="" action="" method="POST">
       <label for="name">Nom :</label><br>
+      <span class="alert"><?php if(!empty($error['name'])) {echo $error['name']; } ?></span>
       <input type="text" name="name" value=""><br>
       <label for="prenom">Prénom : </label><br>
+      <span class="alert"><?php if(!empty($error['prenom'])) {echo $error['prenom']; } ?></span>
       <input type="text" name="prenom" value=""><br>
       <input type="submit" name="submit" value="ASV ?">
     </form>
